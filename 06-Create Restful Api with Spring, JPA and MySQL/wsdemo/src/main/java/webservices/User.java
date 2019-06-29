d15aa6c6b714acfd617eb63c258aabec5607ecf7
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-06-29 07:07:58 +0700

    update

---
+++ 06-Create Restful Api with Spring, JPA and MySQL/wsdemo/src/main/java/webservices/User.java
@@ -0,0 +1,64 @@
+package webservices;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity //Hibernate's entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String email;
+    private String userType; //facebook / default
+    private String password;//You should save encrypted password
+
+    public User(String name, String email, String userType, String password) {
+        this.name = name;
+        this.email = email;
+        this.userType = userType;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
