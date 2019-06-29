e1aab55e0f6b8882070b9b143e71a8dbb6666554
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-06-29 17:20:37 +0700

    update

--- 06-Create Restful Api with Spring, JPA and MySQL/wsdemo/src/main/java/webservices/Application.java
+++ 06-Create Restful Api with Spring, JPA and MySQL/wsdemo/src/main/java/webservices/Application.java
@@ -1,5 +1,14 @@
 package webservices;
+/*
 
+/Applications/XAMPP/bin/mysql -u root -p
+create database wstutorials;
+create user 'springuser'@'%' identified by '123456';
+grant all on wstutorials.* to 'springuser'@'%';
+
+Connect:
+/Applications/XAMPP/bin/mysql -u springuser -p -h 192.168.1.34
+*/
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
