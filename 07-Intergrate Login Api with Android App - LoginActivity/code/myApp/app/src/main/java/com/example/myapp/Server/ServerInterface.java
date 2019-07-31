4b1bf3afee8d6f8a1a9c608360780dfdd2c265ac
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-07-31 11:20:38 +0700

    update

--- 07-Intergrate Login Api with Android App - LoginActivity/code/myApp/app/src/main/java/com/example/myapp/Server/ServerInterface.java
+++ 07-Intergrate Login Api with Android App - LoginActivity/code/myApp/app/src/main/java/com/example/myapp/Server/ServerInterface.java
@@ -2,4 +2,5 @@ package com.example.myapp.Server;
 
 public interface ServerInterface {
     public void loginWithEmailAndPassword(String email, String password);
+    public void queryPlaces(Integer userId, Integer offset, Integer limit);
 }
--- 08/code/wsdemo/src/main/java/webservices/ResponseObject.java
+++ 08/code/wsdemo/src/main/java/webservices/ResponseObject.java
@@ -6,7 +6,7 @@ public class ResponseObject {
     //factory method
     public static Hashtable<String, Object> create(String status, Object data, String message) {
         Hashtable<String, Object> hashtable = new Hashtable<>();
-        hashtable.put("status", status);
+        hashtable.put("result", status);
         hashtable.put("data", data);
         hashtable.put("message", message);
         return hashtable;
